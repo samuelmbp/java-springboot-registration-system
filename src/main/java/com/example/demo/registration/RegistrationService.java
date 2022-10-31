b709a0ebfa563a90c888c759c6bac973a90d3851
Samuel Raducan <samuel.thembp@gmail.com>
2022-10-31 15:53:41 +0000

    Add updateConfirmedAt in the confirmationTokenRepository interface and setConfirmedAt method in confirmationTokenService class

--- src/main/java/com/example/demo/registration/RegistrationService.java
+++ src/main/java/com/example/demo/registration/RegistrationService.java
@@ -3,14 +3,21 @@ package com.example.demo.registration;
 import com.example.demo.appuser.AppUser;
 import com.example.demo.appuser.AppUserRole;
 import com.example.demo.appuser.AppUserService;
+import com.example.demo.registration.token.ConfirmationToken;
+import com.example.demo.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service
 @AllArgsConstructor
 public class RegistrationService {
     private final EmailValidator emailValidator;
     private final AppUserService appUserService;
+    private final ConfirmationToken confirmationToken;
+    private final ConfirmationTokenService confirmationTokenService;
 
     public String register(RegistrationRequest request) {
         boolean isValidEmail = emailValidator.test(request.getEmail());
