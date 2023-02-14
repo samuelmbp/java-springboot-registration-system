4e100816a302670e9f10fcb1c22d6b566ba5d70c
Samuel Raducan <samuel.thembp@gmail.com>
2023-02-14 17:10:06 +0000

    Clean up unused imports and images

--- src/main/java/com/example/demo/security/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/security/config/WebSecurityConfig.java
@@ -1,13 +1,11 @@
 package com.example.demo.security.config;
 
 import com.example.demo.appuser.AppUserService;
-import com.example.demo.security.PasswordEncoder;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
